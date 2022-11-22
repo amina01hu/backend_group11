c3f1d7d3726ba0babd56a7b6b24e5f4887218f02
Amina H <amina01hu@gmail.com>
2022-11-21 19:31:09 -0500

    F

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
-const uri = "mongodb+srv://dbAdmin:dbAdminPassword@cluster0.antlr.mongodb.net/BlogDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbAdmin:dbAdminPassword@cluster0.antlr.mongodb.net/Blog-Database?retryWrites=true&w=majority";
 
-const { Console } = require("console");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 let User = require('./modules/user-schema');
+let Post = require('./modules/post-schema');
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("Database connected!");
 });
 
 const db = mongoose.connection;
-console.log(db.readyState);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get('/', (req, res) => {
     res.send("Hello Everyone!");
-    const result = User.create({
-        "username" :"AminaTesting",
-        "password":"Testing",
-        "security_questions":[ "my cat"],
-        "fName":"Amina",
-        "lName":"Hussein",
-        "img":"fake image",
-        "desc":"bruh"
-    }).then(function(result){
-        console.log(result)
-      });
-
-    console.log(result);
 })
 
 app.get('/users', (req, res) => {
-    res.send("TESTING!");
+    res.send("Searching for user AminaTesting!");
     let id = "AminaTesting";
     const result = User.findOne({username: id}).exec().then(function(result){
-        console.log(result)
+        console.log(result);
       })
 })
 
 app.get('/usersList', (req, res) => {
-    res.send("TESTING!");
+    res.send("Searching for all User records !");
     const result = User.find().exec().then(function(result){
         console.log(result)
       })
 })
+
+app.get('/posts', (req, res) => {
+    res.send("Searching for post by AminaTesting!");
+    let id = "AminaTesting";
+    const result = Post.findOne({username: id}).exec().then(function(result){
+        console.log(result)
+      })
+  })
+  
+  app.get('/postsList', (req, res) => {
+    res.send("Searching for all User posts!");
+    const result = Post.find().exec().then(function(result){
+        console.log(result)
+      })
+  })
 
 
 app.listen(HTTP_PORT, () => {
--- modules/database-connections.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise; // Added to get around the deprecation warning: "Mongoose: promise (mongoose's default promise library) is deprecated"
-
-// Load the schema
-const userSchema = require('./user-schema.js');
-
-

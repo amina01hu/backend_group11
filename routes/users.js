d56b77db16741eb2423a388b5126dfca6240b17a
Amina H <amina01hu@gmail.com>
2023-02-03 01:22:35 -0500

    bruh

--- routes/users.js
+++ routes/users.js
@@ -53,7 +53,7 @@ router.route('/login').post(async (req, res) => {
         }else{
             res.send("Account not found")
         }
-    }).catch(err => res.json("Account not found"))
+    }).catch(err => res.json("Account error"))
     
 })
 
